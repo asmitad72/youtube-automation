7ae158254cc155b14d7d4f96a18168b1e5e50f00
Asmita Kharat <asmitad72@gmail.com>
2024-04-26 13:59:14 +0300

    Added test scripts in package.json

--- cypress/integration/channelsubscrption.js
+++ cypress/integration/channelsubscrption.js
@@ -51,5 +51,33 @@ describe('YouTube Channel Navigation', () => {
         .find('#avatar-section')
         .eq(1)
         .click();
+       
     });
+
+    it('Verify Channel Page', () => {
+        // Navigate to a channel page (replace URL with actual channel page URL)
+        cy.visit('https://www.youtube.com/@GoldminesTelefilms');
+        // Verify channel details are displayed correctly
+        cy.get('body > ytd-app:nth-child(4) > div:nth-child(6) > ytd-page-manager:nth-child(4) > ytd-browse:nth-child(1) > div:nth-child(3) > ytd-c4-tabbed-header-renderer:nth-child(1) > tp-yt-app-header-layout:nth-child(1) > div:nth-child(1) > tp-yt-app-header:nth-child(1) > div:nth-child(1) > div:nth-child(5) > div:nth-child(1) > div:nth-child(1) > div:nth-child(3) > div:nth-child(1) > ytd-channel-name:nth-child(1) > div:nth-child(1) > div:nth-child(1) > yt-formatted-string:nth-child(1)').should('contain.text', 'Goldmines');
+        
+    });
+  
+    it('Subscribe to the Channel', () => {
+        // Navigate to a channel page (replace URL with actual channel page URL)
+        cy.visit('https://www.youtube.com/@GoldminesTelefilms');
+        // Click on the ""Subscribe"" button
+        cy.get('#subscribe-button > .style-scope > yt-button-shape > .yt-spec-button-shape-next > yt-touch-feedback-shape > .yt-spec-touch-feedback-shape > .yt-spec-touch-feedback-shape__fill').click();
+        //Verify subscribe button is clicked
+        cy.get('ytd-modal-with-title-and-button-renderer.style-scope').should('exist').and('be.visible');
+        //fail test case
+        //cy.get('ytd-modal-with-title-and-button-renderer.style-scope').should('not.exist').and('not.be.visible');
+
+        // Verify user is subscribed to the channel (add assertions as needed)
+     });
+
+
+
+
+
+
 });
